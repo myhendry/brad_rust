aa3eb8b9c8014206f9b9190429c6e3f374691801
Hendry Lim <myhendry@gmail.com>
2019-03-03 15:19:06 +0800

    up to T16 references

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,67 @@
+#[allow(dead_code)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn main() {
-    let x = 5;
-    let y = x;
-    println!("{}, {}", x, y);
+    //  single line comment
+    /*
+        multi line comment
+        multi line comment
+    */
+    let i: i32 = 5;
+    let f: f32 = 3.6;
+    let b: bool = false;
+    let d: Direction = Direction::Left;
+    const MAX_NUMBER: u32 = 9;
+    let t: (u32, f32, u32, (i32, i32)) = (1, 2.3, 5, (3, 7));
 
-    let a = String::from("Hello");
-    let b = &a;
-    println!("{}, {}", a, b);
+    // USING REFERENCES T16
+    let mut m = 10;
+    // mr is a mutable reference of m
+    let mr = &mut m;
+    *mr += 1;
+    println!("m is {}", m);
 
-    let c = check();
-    println!("{}", c)
+    // using enums
+    match d {
+        Direction::Up => println!("Going Up"),
+        Direction::Down => println!("Going Down"),
+        Direction::Left => println!("Going Left"),
+        Direction::Right => println!("Going Right"),
+    }
+
+    // using constants
+    for n in 1..MAX_NUMBER {
+        println!("{}", n)
+    }
+
+    // accessing nested tupple
+    println!("{}", (t.3).0);
+
+    // using function with NO return type
+    shout(5);
+
+    // using function with return type
+    let res: bool = is_even(21);
+
+    // println! primitive values
+    println!("{} {} {} {} {}", i, f, b, MAX_NUMBER, res);
+
+    // println! tuple
+    println!("{:?}", t);
 }
 
-fn check() -> String {
-    let s = String::from("hey");
-    s
+// function with primitive as args
+fn shout(num: u32) {
+    for n in 1..num {
+        println!("{}", n)
+    }
 }
 
-// fn pass(c: u32) -> u32 {
-//     println!("{}", c);
-//     c;
-// }
-
-// return void?
+fn is_even(num: u32) -> bool {
+    num % 2 == 0
+}
